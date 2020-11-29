0c38eae3d75da62be766bbfb8ed7a82860599791
mello1994 <neoenda@gmail.com>
2020-11-29 14:33:23 +0900

    implement display-new-message

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -17,7 +17,7 @@ const MessageField = ({inputEl, name, setText, text}) => {
                 const text = e.target.value;
                 if(text === '') return;
                 if(e.key === 'Enter'){
-                    pushMessage({name: 'なおゆき', text});
+                    pushMessage({name, text});
                     setText('');
                     e.preventDefault();
                 }
